c901be3a432bd8d24a499900506cc55d732d16e1
TheMohitBuilds <mohit120577@gmail.com>
2026-06-22 00:27:25 +0530

    Added python input function practice folder

---
+++ Python-input_function/main.py
@@ -0,0 +1,9 @@
+a = input("Enter your name: ")
+print("My name is", a)
+
+b = input("Enter your age: ")
+print("My age is", b)
+
+x = input("enter 1st number: ")
+y = input("enter 2nd number: ")
+print(int(x) + int(y))
