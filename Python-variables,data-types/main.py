4c7fb6c92975b4234c880691077bc1b77d7a457f
TheMohitBuilds <mohit120577@gmail.com>
2026-06-21 23:31:45 +0530

    Added variables and datatypes practice folder

---
+++ Python-variables,data-types/main.py
@@ -0,0 +1,31 @@
+#Variables and Data-Types
+
+a = 125
+a1 = 77
+b = True
+c = "Mohit"
+d = complex(5,7)
+e = 7.7
+
+print(a+a1)
+print(b)
+print(c)
+print(d)
+print(e)
+
+
+print("The type of a is", type(a))
+print("The type of b is", type(b))
+print("The type of c is", type(c))
+print("The type of c is", type(d))
+print("The type of c is", type(e))
+
+
+list1 = [7, 12], [4, 23], [10, 14]
+print(list1)
+
+tuple1 = (("Lion", "Tiger"), ("Cat", "Dog"))
+print(tuple1)
+
+dict1 = {"Name":"Mohit", "Age":"17", "goal":"Full-Stack-AI-Engineer"}
+print(dict1)
