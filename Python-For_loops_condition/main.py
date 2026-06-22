9abc5e0ca5316e6f89433d9c52edf9b7f3ada1f3
TheMohitBuilds <mohit120577@gmail.com>
2026-06-22 14:03:09 +0530

    Added for-loop condition practice folder

---
+++ Python-For_loops_condition/main.py
@@ -0,0 +1,21 @@
+#name = 'TheMohitBuilds'
+#for i in name:
+  #print(i)
+  #if(i=="s"): 
+   #print("[Full-Stack-AI-Engineer]")
+
+#for k in range(7):
+ #print(k)
+
+#for k in range(7):
+ #print(k+1)
+
+#for k in range(1,30,3):
+ #print(k)
+
+#for k in range(1,125):
+ #print(k)
+
+for k in range(1,125):
+ print(k+1)
+
