cdd71ece34ce04d8db789a1f95b8cf06526f5138
TheMohitBuilds <mohit120577@gmail.com>
2026-06-22 12:52:55 +0530

    Added if-elif-else conditions practice folder

---
+++ Python-if_else_condition/if-elif-else.py
@@ -0,0 +1,15 @@
+num = int(input("Enter the value of num: "))
+
+if(num<0):
+    print("number is negative")
+
+elif(num==0):
+    print("number is zero")
+
+elif(num==777):
+    print("number is special")
+
+else:
+    print("number is positive")
+
+print("TheMohitBuilds~")
