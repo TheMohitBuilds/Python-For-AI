dcd04b889d9ad929741acae20d8c4e61bca07895
TheMohitBuilds <mohit120577@gmail.com>
2026-06-22 14:46:58 +0530

    Added while-loop condition practice folder

---
+++ Python-while_loop_condition/main.py
@@ -0,0 +1,33 @@
+#while loop
+
+#i = 0
+#while(i<=49): 
+  
+  #i = int(input("Enter the number: "))
+  #print(i)
+#print("[Done with the loop]")
+
+#count = 7
+#while(count>0):
+   #print(count)
+    #count = count - 1
+#print("[Done with the loop]")
+
+
+#Else with the while loop
+
+i = 0
+while(i<=99):
+    i = int(input("Enter the number: "))
+    print(i)
+
+else:
+  print("It is a special number")
+
+
+
+
+
+
+
+
